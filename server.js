1cc477d0551be4bebf609e9e0c3a745121752e00
Aditya Chaudhari <adi.chaudhari1997@gmail.com>
2022-11-05 12:41:15 +0530

    removed options

--- server.js
+++ server.js
@@ -8,7 +8,6 @@ const PORT = process.env.PORT || 5000;
 require("./db/conn"); //link database
 app.use(express.json());
 app.use(cors);
-app.options('*', cors());
 app.use(require('./router/auth')); //link router files
 // const corsOptions = {
     //     origin: true,
